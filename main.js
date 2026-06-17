e3adcfb0e42e538c21dd1fee1e55c1ac3bac138c
soft-compile <ezyjsdeveloper@gmail.com>
2026-06-17 19:53:59 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 "use strict";
+const state = {
+	dopamine: 0.5,
+	memory: [],
+	short_urge: 0.9,
+	long_urge: 0.5,
+};
+const $ = document.querySelector.bind(document);
+const spirit = $("#spirit");
