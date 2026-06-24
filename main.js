c7a9329cfe042069914f150aae06a38e31c5d4ca
soft-compile <ezyjsdeveloper@gmail.com>
2026-06-24 21:02:46 +0800

    fix

--- main.js
+++ main.js
@@ -696,7 +696,7 @@ function main() {
 	state.current_thought.update_delta_dopamine(
 		caculate_dopamine() - state.previous_dopamine
 	);
-	if (destination.step--) {
+	if (destination.step-- > 0) {
 		if (destination.dx === 0 && destination.dy === 0) {
 			destination.step = 0;
 		} else {
